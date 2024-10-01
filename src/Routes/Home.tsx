51f8623de87027e3b3994358fd3170369fb007a6
veenoise <william.eduard123@gmail.com>
2024-10-01 14:03:53 +0800

    changed ui for the /home endpoint

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -6,21 +6,27 @@ const Home = () => {
   return (
     <div className="min-h-[calc(100dvh-64px)] container mx-auto">
       <h2 className="text-center text-2xl m-5">Python++</h2>
-      <Tabs defaultValue="input">
-        <TabsList className="flex">
-          <TabsTrigger value="input">Input</TabsTrigger>
-          <TabsTrigger value="output">Output</TabsTrigger>
-        </TabsList>
-        <TabsContent value="input">
-          <Textarea placeholder="Type your code here." className="resize-none mb-4" rows={25} />
-          <div className="flex justify-end">
-            <Button className="bg-[#FFDD6F] text-neutral-800 hover:bg-[#FFDD40] hover:text-neutral-700">Analyze</Button>  
+      <div className="border rounded-md p-4 bg-neutral-800">
+        <Tabs defaultValue="input">
+          <div className="w-full flex justify-center">
+            <TabsList className="bg-neutral-300">
+              <TabsTrigger value="input">Input</TabsTrigger>
+              <TabsTrigger value="output">Output</TabsTrigger>
+            </TabsList>
           </div>
-        </TabsContent>
-        <TabsContent value="output">
-          Here is the output
-        </TabsContent>
-      </Tabs>
+          
+          <TabsContent value="input">
+            <Textarea placeholder="Type your code here." className="resize-none mb-4" rows={25} />
+            <div className="flex justify-end">
+              <Button className="bg-[#FFDD6F] text-neutral-800 hover:bg-[#FFDD40] hover:text-neutral-700">Analyze</Button>  
+            </div>
+          </TabsContent>
+          <TabsContent value="output">
+            <p className="text-white">This is the content</p>
+          </TabsContent>
+        </Tabs>
+      </div>
+      
       
     </div>
   )
